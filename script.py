83316f547e17caa2df8bbec1e37221ab0ac59109
seb-viney <sebastian.r.viney@gmail.com>
2022-09-30 12:19:30 +1000

    testing committ

--- script.py
+++ script.py
@@ -8,3 +8,31 @@ data_foder = r'G:\GitHub\work_development\kaggle\kaggle_titanic_ml\data'
 train_csv = data_foder + r'\train.csv'
 test_csv = data_foder + r'\test.csv'
 
+#  Data Frames
+train_df = pd.read_csv(train_csv)
+test_df = pd.read_csv(test_csv, 
+    usecols=['PassengerId', 
+        'Pclass',
+        'Name',
+        'Sex',
+        'Age',
+        'SibSp',
+        'Parch',
+        'Ticket',
+        'Fare',
+        'Cabin',
+        'Embarked'],
+    dtype={'PassengerId': str,
+        'Pclass': str,
+        'Name': str,
+        'Sex': str,
+        'Age': str,
+        'SibSp': str,
+        'Parch': str,
+        'Ticket': str,
+        'Fare': str,
+        'Cabin': str,
+        'Embarked': str}
+    )
+                                            
+print("testing")
