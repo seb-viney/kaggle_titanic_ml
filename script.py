70d447d0143edd584776033de2d13745f8ff3675
sleboh <sebviney@gmail.com>
2022-09-30 11:54:43 +1000

    Setting up project

---
+++ script.py
@@ -0,0 +1,10 @@
+import os
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+
+#  Folder and Files path locations
+data_foder = r'G:\GitHub\work_development\kaggle\kaggle_titanic_ml\data'
+train_csv = data_foder + r'\train.csv'
+test_csv = data_foder + r'\test.csv'
+
